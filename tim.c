d165d033b91e52c90652da026b94245cac7ef1aa
houhongyu <43408753+houhongyu@users.noreply.github.com>
2018-09-26 18:25:57 +0800

    Update tim.c

--- tim.c
+++ tim.c
@@ -8,42 +8,42 @@ extern unsigned char fd2[];
 extern unsigned char fd3[];
 extern unsigned char fd4[];
 /****************************************************************************
-* ��    �ƣ�void timer_ini(void)
-* ��    �ܣ���ʱ����ʼ������
-* ��ڲ�������
-* ���ڲ�������
-* ˵    ����
-* ���÷������� 
+* 名    称：void timer_ini(void)
+* 功    能：定时器初始化函数
+* 入口参数：无
+* 出口参数：无
+* 说    明：
+* 调用方法：无 
 ****************************************************************************/ 
 void timer_ini(void)
 {
    
-   //��ʱ����Ϊ�˲���250K�ĸ���Ƶ�ʣ� Ҳ����4us�Ķ�ʱ  ���ڳ������Ŀ���
+   //定时器是为了产生250K的更新频率， 也就是4us的定时  用于超声波的控制
    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-   //����TIM2��ʱ��
+   //允许TIM2的时钟
    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2,ENABLE);
-   //���½�Timer����Ϊȱʡֵ
+   //重新将Timer设置为缺省值
    TIM_DeInit(TIM2);
-   //�����ڲ�ʱ�Ӹ�TIM2�ṩʱ��Դ
+   //采用内部时钟给TIM2提供时钟源
    TIM_InternalClockConfig(TIM2);
-   //Ԥ��Ƶϵ��Ϊ36000-1������������ʱ��Ϊ72MHz/144 = 500KHz   
+   //预分频系数为36000-1，这样计数器时钟为72MHz/144 = 500KHz   
    TIM_TimeBaseStructure.TIM_Prescaler = 144 - 1;
-   //����ʱ�ӷָ�
+   //设置时钟分割
    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-   //���ü�����ģʽΪ���ϼ���ģʽ
+   //设置计数器模式为向上计数模式
    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-   //���ü��������С��ÿ��2�����Ͳ���һ�������¼�
+   //设置计数溢出大小，每计2个数就产生一个更新事件
    TIM_TimeBaseStructure.TIM_Period = 2-1;
-   //������Ӧ�õ�TIM2��
+   //将配置应用到TIM2中
    TIM_TimeBaseInit(TIM2,&TIM_TimeBaseStructure);
-   //�������жϱ�־
+   //清除溢出中断标志
    TIM_ClearFlag(TIM2, TIM_FLAG_Update);
-   //��ֹARRԤװ�ػ�����
+   //禁止ARR预装载缓冲器
    TIM_ARRPreloadConfig(TIM2, DISABLE);
-   //����TIM2���ж�
+   //开启TIM2的中断
    TIM_ITConfig(TIM2,TIM_IT_Update,ENABLE);
    TIM_Cmd(TIM2,ENABLE);
 }
 
-/******************* (C) COPYRIGHT 2011 �ܶ�STM32 *****END OF FILE****/
+/******************* (C) COPYRIGHT STM32 *****END OF FILE****/
 
